da06e57db4268eb30a017653e762a95e81697838
Eztekto <iztekto@gmail.com>
2021-11-28 21:42:35 +0300

    Изменена реализация 4го пункта

--- src/main/java/pages/AbstractPage.java
+++ src/main/java/pages/AbstractPage.java
@@ -2,6 +2,7 @@ package pages;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.PageFactory;
 
 import java.io.IOException;
 
@@ -13,4 +14,6 @@ public abstract class AbstractPage {
     public static void setDriver(WebDriver webDriver) {
         driver = webDriver;
     }
+
+
 }
--- src/main/java/pages/TicketsPage.java
+++ src/main/java/pages/TicketsPage.java
@@ -1,30 +1,23 @@
 package pages;
 
+import org.junit.Assert;
 import org.openqa.selenium.By;
-import org.openqa.selenium.NoSuchElementException;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.support.ui.Select;
-
-import java.util.List;
 
 public class TicketsPage extends AbstractPage {
 
 
     private WebElement searchQuery = driver.findElement(By.id("search_query"));
 
-    public void ticketSearch(String problem){
+    public void ticketSearch(String problem) {
         this.searchQuery.sendKeys(problem);
         WebElement searchButton = driver.findElement(By.xpath("//*[@id=\"searchform\"]/div/div/button"));
         searchButton.click();
-        List<WebElement> webElementList = driver.findElements(By.xpath("//*[@id=\"ticketTable\"]"));
-        for (WebElement webElement : webElementList) {
-            try {
-                System.out.println("Тикет " + webElement.findElement(By.xpath("//*[@id=\"ticketTable\"]/tbody/tr[1]/td[2]")).getText());
-            } catch (NoSuchElementException e) {
-                System.out.println("Тикета не найден");
-            }
-        }
-
+        WebElement titleElement = driver.findElement(By.xpath("//tr[@class]//a"));
+        Assert.assertTrue("Нет тикета",titleElement.getText().contains(problem));
 
     }
+
+
 }
+
